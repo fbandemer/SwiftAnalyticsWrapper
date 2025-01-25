a5f8954d65b616833b6ee6cc589ce6a9fcacb76f
fbandemer <fynn.bandemer@gmail.com>
2025-01-25 11:54:19 +0100

    ScreenCaptureModifier added

---
+++ Sources/Analytics/Views/ScreenCaptureModifier.swift
@@ -0,0 +1,23 @@
+//
+//  ScreenCaptureModifier.swift
+//  Analytics
+//
+//  Created by Fynn Bandemer on 1/25/25.
+//
+
+import PostHog
+import SwiftUI
+
+struct ScreenCaptureModifier: ViewModifier {
+    let title: String
+    func body(content: Content) -> some View {
+        content
+            .postHogScreenView(title)
+    }
+}
+
+public extension View {
+    func postHogScreenView(_ title: String) -> some View {
+        modifier(ScreenCaptureModifier(title: title))
+    }
+}
