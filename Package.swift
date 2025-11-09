5c66efb04c8b91b33cf0f7ef1ddc7ce3096fd019
fbandemer <39947411+fbandemer@users.noreply.github.com>
2025-11-09 16:50:46 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .package(url: "https://github.com/RevenueCat/purchases-ios.git", from: "5.2.0"),
         .package(url: "https://github.com/PostHog/posthog-ios.git", from: "3.0.0"),
         .package(url: "https://github.com/apple/swift-testing.git", from: "0.9.0"),
-        .package(url: "https://github.com/fbandemer/SwiftAnalyticsKitInterface", branch: "main")
+        .package(url: "https://github.com/fbandemer/SwiftAnalyticsKitInterface", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
